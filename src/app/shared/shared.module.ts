34eb8f2b81d1bc8751050e3ebada91245917c502
Danilo Caro <danilocaro23@gmail.com>
2022-10-05 10:49:14 -0500

    🎉 Initial setup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from '@shared/pages/not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
